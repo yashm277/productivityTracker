953c8ad5fc0ad86a14be6dc61b024299f1bc1949
relatrino-code <code.relatrino@gmail.com>
2022-12-04 21:03:50 +0530

    again tasks.java shit

--- src/productivityTracker/thtask.java
+++ src/productivityTracker/thtask.java
@@ -16,7 +16,7 @@ public class thtask extends javax.swing.JFrame {
     public thtask() {
         initComponents();
     }
-    int th=0;
+    int n=0;
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
